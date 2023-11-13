2420b24051d79064120b4ea15ac43381b2341372
ALHarbiAF <143851806+ALHarbiAF@users.noreply.github.com>
2023-11-13 19:08:10 +0300

    Update script.js

--- Exercise7-js/script.js
+++ Exercise7-js/script.js
@@ -1,14 +1,76 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    var canvas = document.getElementById('signature-pad');
+    var clearButton = document.getElementById('clear-pad');
+    var signatureInput = document.getElementById('signature-data');
+    var ctx = canvas.getContext('2d');
+    var drawing = false;
+    var lastX = 0;
+    var lastY = 0;
+
+    function startDrawing(e) {
+        drawing = true;
+        lastX = e.clientX - canvas.offsetLeft;
+        lastY = e.clientY - canvas.offsetTop;
+    }
+
+    function stopDrawing() {
+        drawing = false;
+       
+        signatureInput.value = canvas.toDataURL();
+    }
+
+    function draw(e) {
+        if (!drawing) return;
+
+        var x = e.clientX - canvas.offsetLeft;
+        var y = e.clientY - canvas.offsetTop;
+
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+
+        lastX = x;
+        lastY = y;
+    }
+
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseout', stopDrawing);
+
+    clearButton.addEventListener('click', function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        signatureInput.value = '';
+    });
+
+   
+    canvas.addEventListener('touchstart', function(e) {
+        var touch = e.touches[0];
+        startDrawing(touch);
+    });
+
+    canvas.addEventListener('touchmove', function(e) {
+        var touch = e.touches[0];
+        draw(touch);
+    });
+
+    canvas.addEventListener('touchend', stopDrawing);
+    canvas.addEventListener('touchcancel', stopDrawing);
+});
+
+
 let turn = "X";
 let gameEnded = false;
 let cellValues = Array(9).fill(null);
+let currentSelectedIndex = 0;
 
-// Utility functions
 function checkWinner() {
     const winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], 
         [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-        [0, 4, 8], [2, 4, 6]            
+        [0, 4, 8], [2, 4, 6]             
     ];
 
     for (let combo of winningCombinations) {
@@ -22,11 +84,23 @@ function checkWinner() {
     return false;
 }
 
+function updateCurrentSelectedCell(oldIndex, newIndex) {
+    cells[oldIndex].classList.remove('selected');
+    cells[newIndex].classList.add('selected');
+}
+
 function checkDraw() {
     return cellValues.every(value => value !== null);
 }
 
-// Event listeners
+
+function updateCurrentSelectedCell(newIndex) {
+    
+    cells.forEach(cell => cell.classList.remove('selected'));
+   
+    cells[newIndex].classList.add('selected');
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const cells = document.getElementsByClassName("cell");
     const board = document.querySelector(".board");
@@ -43,7 +117,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
         cell.innerText = turn;
         cellValues[index] = turn;
-        cell.style.backgroundColor = turn === "X" ? "#ADD8E6" : "#90EE90";  
+        cell.style.backgroundColor = turn === "X" ? "#ADD8E6" : "#90EE90"; 
+
         if (!checkWinner()) {
             if (checkDraw()) {
                 document.getElementById("turn").innerText = "Draw!";
@@ -55,17 +130,83 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    resetButton.addEventListener("click", function () {
-        cellValues.fill(null);
-        gameEnded = false;
-        turn = "X";
-        turnDisplay.innerText = turn;
-        document.getElementById("turn").innerText = "Now it's " + turn + "'s turn";
+document.addEventListener('keydown', function(event) {
+    if (gameEnded) return;
 
-        Array.from(cells).forEach(cell => {
-            cell.innerText = '';
-            cell.style.backgroundColor = "beige";
-            cell.classList.remove("winner");
-        });
-    });
+    let handled = false;
+
+    switch (event.key) {
+        case 'ArrowLeft':
+            if (currentSelectedIndex % 3 > 0) {
+                currentSelectedIndex--;
+                handled = true;
+            }
+            break;
+        case 'ArrowRight':
+      
+            if (currentSelectedIndex % 3 < 2) {
+                currentSelectedIndex++;
+                handled = true;
+            }
+            break;
+        case 'ArrowUp':
+         
+            if (currentSelectedIndex > 2) {
+                currentSelectedIndex -= 3;
+                handled = true;
+            }
+            break;
+        case 'ArrowDown':
+           
+            if (currentSelectedIndex < 6) {
+                currentSelectedIndex += 3;
+                handled = true;
+            }
+            break;
+        case 'Enter':
+           
+            if (cellValues[currentSelectedIndex] === null && !cells[currentSelectedIndex].classList.contains('winner')) {
+                cells[currentSelectedIndex].click();
+            }
+            handled = true;
+            break;
+    }
+
+    if (handled) {
+        updateCurrentSelectedCell();
+        event.preventDefault(); 
+    }
 });
+
+
+function updateCurrentSelectedCell() {
+
+    Array.from(cells).forEach(cell => cell.classList.remove('selected'));
+    
+    cells[currentSelectedIndex].classList.add('selected');
+}
+});
+
+
+function resetGame() {
+    cellValues = Array(9).fill(null); 
+    gameEnded = false; 
+    turn = "X"; 
+    currentSelectedIndex = 0; 
+    document.getElementById("turn").innerText = "Now it's X's turn"; 
+    
+    Array.from(cells).forEach(cell => {
+        cell.innerText = '';
+        cell.style.backgroundColor = ''; 
+        cell.classList.remove("winner", "selected"); 
+    });
+    
+  
+    updateCurrentSelectedCell();
+}
+
+
+document.getElementById("reset").addEventListener("click", resetGame);
+const cells = document.getElementsByClassName("cell");
+updateCurrentSelectedCell(0);
+
